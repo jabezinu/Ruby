455d2717f266168b6ff0e22699bd72cd4e814cdf
Jabez Shiferaw <jabezzinu31@gmail.com>
2025-03-29 10:35:50 +0300

    map, select and reject

---
+++ loop.rb
@@ -0,0 +1,16 @@
+# map
+# x = [1, 2, 3].map{|n| n*2}
+# puts x
+
+# select
+# even_numbers = numbers.select { |num| num.even? }
+# puts even_numbers.inspect  # Output: [2, 4, 6]
+
+# reject
+numbers = [1, 2, 3, 4, 5, 6]
+# find
+
+# inject/Reduce
+
+
+
